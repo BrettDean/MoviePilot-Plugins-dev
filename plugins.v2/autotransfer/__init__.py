a4f96561df6360d66ddace841038aa5e9df68a40
Dean <dean@example.com>
2026-02-13 12:43:38 +0800

    chore(autotransfer): 版本号更新至1.0.50

--- plugins.v2/autotransfer/__init__.py
+++ plugins.v2/autotransfer/__init__.py
@@ -53,7 +53,7 @@ class autoTransfer(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/BrettDean/MoviePilot-Plugins/main/icons/autotransfer.png"
     # 插件版本
-    plugin_version = "1.0.49"
+    plugin_version = "1.0.50"
     # 插件作者
     plugin_author = "Dean"
     # 作者主页
